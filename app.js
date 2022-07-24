61bedb41c2018c0b64c6e9a9d562bf96761db6d3
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-07-24 12:54:08 -0500

    added focus state, styled theme btn

--- app.js
+++ app.js
@@ -1,82 +1,139 @@
+///////////////////////////////////////////
+
 const body = document.querySelector('body');
 const darkModeBtn = document.getElementById('dark-mode-button');
 const inputItem = document.getElementById('item');
 const addItemBtn = document.getElementById('add-item-button');
 const ul = document.querySelector('.ul');
-const li = document.querySelector('li')
+const li = document.querySelector('li');
+const lis = document.querySelectorAll('li');
 const itemsArr = [];
-const isCheckedArr = [];
 
-darkModeBtn.addEventListener('click', function () {
-    body.classList.toggle('dark-theme');
-});
+// Set local storage for theme
+localStorage.setItem('darkMode', 'false')
 
+// Dark mode toggle
+darkModeBtn.addEventListener('click', function() {
+    body.classList.toggle('dark-theme')
+    
+    for (let li of lis) {
+        li.classList.toggle('dark-mode')
+    }
+    // reset local storage
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('darkMode', 'true')
+    } else {
+        localStorage.setItem('darkMode', 'false')
+    }
+})
+
+// Mouseover
+darkModeBtn.addEventListener('mouseover', (e) => {
+    let mode = localStorage.getItem('darkMode');
+
+    if ( mode === 'true') {
+        darkModeBtn.innerText = 'Light'
+        darkModeBtn.style.color = '#f5f5f5'
+    } else {
+        darkModeBtn.innerText = 'Dark'
+        darkModeBtn.style.color = '#212121'
+    }
+})
+
+// Mouseout
+darkModeBtn.addEventListener('mouseout', (e) => {
+    darkModeBtn.innerText='Theme'
+    darkModeBtn.style.color = '#3f3f3f'
+})
+
+// Add button pushes input value to itemsArr
 addItemBtn.addEventListener('click', function () {
     if (inputItem.value === '') {
         alert('Please enter an item!');
     } else {
         itemsArr.push({
             'value': `${inputItem.value}`,
-            'isChecked': 'false'
+            'newVal': `${inputItem.value.replace(/\s+/g, '')}`,
+            'isChecked': 'false' // boolean for checked state
         });
         inputItem.value = '';
         renderItems(itemsArr);
     }
 })
 
+// Check items (before render)
 function checkTodoItem() {
     const checkedBtns = document.querySelectorAll('.items-text'); //<p> elements
     const lis = document.querySelectorAll('.rendered-items'); //<li> elements
-    console.log(checkedBtns, lis)
+    
     for(let i=0; i<checkedBtns.length; i++) {
+
         checkedBtns[i].addEventListener('click', function() {
-            if(checkedBtns[i].id == itemsArr[i].value) {
+            lis[i].classList.toggle('checked')
+
+            if (lis[i].classList.contains('checked')) {
                 itemsArr[i].isChecked = 'true';
-                lis[i].classList.add('checked');
+            } else {
+                itemsArr[i].isChecked = 'false';
+            }
+        })  
+    }
+
+    checkedBtns.forEach(chkBtn => {
+        chkBtn.addEventListener('keyup', function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                chkBtn.click();
             }
         })
+    });
+}
+
+// Is it checked? (add checked class after render)
+function isChecked() {
+    const checkedBtns = document.querySelectorAll('.items-text'); //<p> elements
+    const lis = document.querySelectorAll('.rendered-items'); //<li> elements
+
+    for (let i=0; i<itemsArr.length; i++) {
+
+            if (itemsArr[i].isChecked === 'true') {
+                lis[i].classList.add('checked');
+            } else if(itemsArr[i].isChecked = 'false') {
+                lis[i].classList.remove('checked');
+            }
     }
 }
 
-function isChecked(a,b) {
-
-}
-
+// Render items
 function renderItems(items) {
     let listItems = '';
 
     for (let i = 0; i < items.length; i++) {
         listItems += `
             <li id="${items[i].value}" class="rendered-items">
-                <p class="items-text" id="${items[i].value}">${items[i].value}</p>
-                <button id=${items[i].value} class="delete-button">DELETE</button>
+                <p tabindex="0" role="button" class="items-text" id="${items[i].value}">${items[i].value}</p>
+                <button id=${items[i].value.replace(/\s+/g, '') } class="delete-button">DELETE</button>
             </li>
         `;
     }
-
     ul.innerHTML = listItems;
-    deleteBtns(itemsArr);
-    checkTodoItem()
-    
-    for (let i=0; i<itemsArr.length; i++) {
-        const lis = document.querySelectorAll('li');
 
-        if(itemsArr[i].isChecked == 'true') {
-            lis[i].classList.add('checked');
-        }
-    }
+    deleteBtns(itemsArr);
+    checkTodoItem();
+    isChecked();
 }
 
+// Delete items
 function deleteBtns(items) {
     let removeBtns = document.querySelectorAll('.delete-button');
 
     for (let i = 0; i < itemsArr.length; i++) {
         removeBtns[i].addEventListener('click', function () {
 
-            let findServiceIndex = (items) => items.value === removeBtns[i].id;
+            let findServiceIndex = (items) => items.newVal === removeBtns[i].id;
             let serviceIndex = items.findIndex(findServiceIndex);
 
-            if (removeBtns[i].id === itemsArr[i].value) {
+            if (removeBtns[i].id === itemsArr[i].newVal) {
                 itemsArr.splice(serviceIndex, 1);
                 renderItems(items);
             }
@@ -84,7 +141,8 @@ function deleteBtns(items) {
     }
 }
 
-inputItem.addEventListener('keyup', function (event) {
+// Enter key for click
+inputItem.addEventListener ('keyup', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         addItemBtn.click();
